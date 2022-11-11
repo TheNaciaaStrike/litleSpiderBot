8aac499688f25792eb5412be78d533f3e9b6bd93
TheNaciaaStrike <51364554+TheNaciaaStrike@users.noreply.github.com>
2022-11-11 20:19:58 +0200

    Hello world now with tests

--- src/main/java/com/thenaciaastrike/litlespiderbot/LitleSpiderBotApplication.java
+++ src/main/java/com/thenaciaastrike/litlespiderbot/LitleSpiderBotApplication.java
@@ -2,12 +2,21 @@ package com.thenaciaastrike.litlespiderbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+@RestController
 public class LitleSpiderBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LitleSpiderBotApplication.class, args);
     }
-
+    @GetMapping("/")
+    public String hello() {
+        return String.format("Hello!");
+    }
 }
--- src/test/java/com/thenaciaastrike/litlespiderbot/LitleSpiderBotApplicationTests.java
+++ src/test/java/com/thenaciaastrike/litlespiderbot/LitleSpiderBotApplicationTests.java
@@ -3,11 +3,19 @@ package com.thenaciaastrike.litlespiderbot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class LitleSpiderBotApplicationTests {
 
+    //@Test
+    //void contextLoads() {
+    //}
     @Test
-    void contextLoads() {
+    public void testHello() {
+        LitleSpiderBotApplication app = new LitleSpiderBotApplication();
+        String result = app.hello();
+        assertEquals("Hello!", result);
     }
 
 }
