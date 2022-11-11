d4f353e2a54504d9331dcbe8d1391315f1e57b17
TheNaciaaStrike <51364554+TheNaciaaStrike@users.noreply.github.com>
2022-11-11 17:27:31 +0200

    Initial commit

---
+++ src/main/java/com/thenaciaastrike/litlespiderbot/LitleSpiderBotApplication.java
@@ -0,0 +1,13 @@
+package com.thenaciaastrike.litlespiderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitleSpiderBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LitleSpiderBotApplication.class, args);
+    }
+
+}
