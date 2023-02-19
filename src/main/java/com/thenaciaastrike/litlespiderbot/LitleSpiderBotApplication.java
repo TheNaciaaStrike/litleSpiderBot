c8468d66c0d4e8bd4503f363c32967c1221e3b97
TheNaciaaStrike <51364554+TheNaciaaStrike@users.noreply.github.com>
2023-02-19 16:45:44 +0200

    removed SQL

--- src/main/java/com/thenaciaastrike/litlespiderbot/LitleSpiderBotApplication.java
+++ src/main/java/com/thenaciaastrike/litlespiderbot/LitleSpiderBotApplication.java
@@ -2,10 +2,12 @@ package com.thenaciaastrike.litlespiderbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @RestController
 public class LitleSpiderBotApplication {
 
